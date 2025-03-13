77e0131fc7fa7864cdcf56624a8dff4013454eb4
duclong565 <nguyenduclong5a1@gmail.com>
2025-03-13 16:23:29 +0700

    add basic crud collaboration for controller

--- src/collaborations/collaborations.controller.ts
+++ src/collaborations/collaborations.controller.ts
@@ -1,6 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CollaborationsService } from './collaborations.service';
 import { Collaboration } from './entity/collaborations.entity';
+import { CreateCollaborationDto } from './dto/create-collaboration.dto';
+import { UpdateCollaborationDto } from './dto/update-collaboration.dto';
 
 @Controller('collaborations')
 export class CollaborationsController {
@@ -9,5 +20,50 @@ export class CollaborationsController {
   @Get()
   getAllCollaborations(): Promise<Collaboration[]> {
     return this.collaborationsService.getAllCollaborations();
+  }
+
+  @Get('/:id')
+  getCollaborationById(@Param('id') id: string): Promise<Collaboration> {
+    return this.collaborationsService.getCollaborationById(id);
+  }
+
+  @Get('/user/:userId')
+  getCollaborationsByUserId(
+    @Param('userId') userId: string,
+  ): Promise<Collaboration[]> {
+    return this.collaborationsService.getCollaborationsByUserId(userId);
+  }
+
+  @Get('/folder/:folderId')
+  getCollaborationsByFolderId(
+    @Param('folderId') folderId: string,
+  ): Promise<Collaboration[]> {
+    return this.collaborationsService.getCollaborationsByProjectId(folderId);
+  }
+
+  @Post()
+  createCollaboration(
+    @Body(ValidationPipe) createCollaborationDto: CreateCollaborationDto,
+  ): Promise<Collaboration> {
+    return this.collaborationsService.createCollaboration(
+      createCollaborationDto,
+    );
+  }
+
+  @Patch('/:id')
+  updateCollaboration(
+    //Validation pipe is used to validate the incoming request body using passed DTO
+    @Param('id') id: string,
+    @Body(ValidationPipe) updateCollaborationDto: UpdateCollaborationDto,
+  ): Promise<Collaboration> {
+    return this.collaborationsService.updateCollaboration(
+      id,
+      updateCollaborationDto,
+    );
+  }
+
+  @Delete('/:id')
+  deleteCollaboration(@Param('id') id: string): Promise<boolean> {
+    return this.collaborationsService.deleteCollaboration(id);
   }
 }
